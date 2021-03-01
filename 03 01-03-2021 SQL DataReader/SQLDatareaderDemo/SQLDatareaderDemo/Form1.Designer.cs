69c8aca1618baca41d8e0c9c2c98f20223f17af7
nirc7 <i2cs2013@gmail.com>
2021-03-01 11:39:10 +0200

    sql reader + sqlcommand

---
+++ 03 01-03-2021 SQL DataReader/SQLDatareaderDemo/SQLDatareaderDemo/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace SQLDatareaderDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtFamily = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lblUsers = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbID = new System.Windows.Forms.RadioButton();
+            this.rdbName = new System.Windows.Forms.RadioButton();
+            this.rdbFamily = new System.Windows.Forms.RadioButton();
+            this.GVUsers = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GVUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(167, 68);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 26);
+            this.txtId.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(167, 114);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtFamily
+            // 
+            this.txtFamily.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFamily.Location = new System.Drawing.Point(167, 163);
+            this.txtFamily.Name = "txtFamily";
+            this.txtFamily.Size = new System.Drawing.Size(100, 26);
+            this.txtFamily.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(96, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(93, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "NAME:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(89, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "FAMILY:";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(167, 211);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(100, 31);
+            this.btnInsert.TabIndex = 6;
+            this.btnInsert.Text = "INSERT";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(167, 257);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 31);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(167, 304);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 31);
+            this.btnUpdate.TabIndex = 8;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // lblUsers
+            // 
+            this.lblUsers.AutoSize = true;
+            this.lblUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsers.Location = new System.Drawing.Point(397, 68);
+            this.lblUsers.Name = "lblUsers";
+            this.lblUsers.Size = new System.Drawing.Size(21, 20);
+            this.lblUsers.TabIndex = 9;
+            this.lblUsers.Text = "...";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(167, 351);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(100, 31);
+            this.btnSelect.TabIndex = 10;
+            this.btnSelect.Text = "SELECT";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbFamily);
+            this.groupBox1.Controls.Add(this.rdbName);
+            this.groupBox1.Controls.Add(this.rdbID);
+            this.groupBox1.Location = new System.Drawing.Point(167, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 49);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "order by";
+            // 
+            // rdbID
+            // 
+            this.rdbID.AutoSize = true;
+            this.rdbID.Checked = true;
+            this.rdbID.Location = new System.Drawing.Point(9, 20);
+            this.rdbID.Name = "rdbID";
+            this.rdbID.Size = new System.Drawing.Size(36, 17);
+            this.rdbID.TabIndex = 0;
+            this.rdbID.TabStop = true;
+            this.rdbID.Text = "ID";
+            this.rdbID.UseVisualStyleBackColor = true;
+            this.rdbID.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // rdbName
+            // 
+            this.rdbName.AutoSize = true;
+            this.rdbName.Location = new System.Drawing.Point(57, 20);
+            this.rdbName.Name = "rdbName";
+            this.rdbName.Size = new System.Drawing.Size(53, 17);
+            this.rdbName.TabIndex = 1;
+            this.rdbName.Text = "Name";
+            this.rdbName.UseVisualStyleBackColor = true;
+            this.rdbName.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // rdbFamily
+            // 
+            this.rdbFamily.AutoSize = true;
+            this.rdbFamily.Location = new System.Drawing.Point(121, 19);
+            this.rdbFamily.Name = "rdbFamily";
+            this.rdbFamily.Size = new System.Drawing.Size(54, 17);
+            this.rdbFamily.TabIndex = 2;
+            this.rdbFamily.Text = "Family";
+            this.rdbFamily.UseVisualStyleBackColor = true;
+            this.rdbFamily.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // GVUsers
+            // 
+            this.GVUsers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.GVUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GVUsers.Location = new System.Drawing.Point(582, 71);
+            this.GVUsers.Name = "GVUsers";
+            this.GVUsers.Size = new System.Drawing.Size(240, 326);
+            this.GVUsers.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 450);
+            this.Controls.Add(this.GVUsers);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lblUsers);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFamily);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GVUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtFamily;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Label lblUsers;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbFamily;
+        private System.Windows.Forms.RadioButton rdbName;
+        private System.Windows.Forms.RadioButton rdbID;
+        private System.Windows.Forms.DataGridView GVUsers;
+    }
+}
+
---
+++ 03 01-03-2021 SQL DataReader/SQLDatareaderDemo/SQLDatareaderDemo/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLDatareaderDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            ExcNQ($"INSERT INTO TBUsers(NAME, FAMILY) VALUES('{txtName.Text}', '{txtFamily.Text}')");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            ExcNQ($" DELETE TBUsers" +
+                    $" WHERE ID={txtId.Text}");
+        }
+
+        private void ExcNQ(string commTXT)
+        {
+            string conStr = @"Data Source=E440\SQLEXPRESS;Initial Catalog=DBUsers;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conStr);
+            //SqlCommand comm = new SqlCommand("INSERT INTO TBUsers(NAME, FAMILY) VALUES('" + txtName.Text + "', '" + txtFamily.Text + "')", con);
+            SqlCommand comm = new SqlCommand(commTXT, con);
+            con.Open();//opt1
+            //comm.Connection.Open();//opt2
+            int res = comm.ExecuteNonQuery(); // Insert, Update, Delete
+            con.Close();
+
+            ShowTBUsers();
+
+            MessageBox.Show((res == 1 ? "" : "not") + "succeeded!");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            ExcNQ($" UPDATE TBUsers " +
+                    $" Set Name='{txtName.Text}' , Family='{txtFamily.Text}'" +
+                    $" WHERE Id={txtId.Text}");
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            ShowTBUsers();
+            ShowGV();
+        }
+
+        private void ShowTBUsers()
+        {
+            string orderby;
+            if (rdbID.Checked)
+                orderby = "ID";
+            else if (rdbName.Checked)
+                orderby = "NAME";
+            else
+                orderby = "Family";
+
+            string output = "";
+            string conStr = @"Data Source=E440\SQLEXPRESS;Initial Catalog=DBUsers;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conStr);
+            SqlCommand comm = new SqlCommand(
+                 " SELECT * " +
+                 " FROM TBUsers " +
+                $" Order By {orderby}", con);
+            con.Open();//opt1
+
+            SqlDataReader reader = comm.ExecuteReader();
+            while (reader.Read())
+            {
+                output += reader[0] + ", " + reader["Name"] + ", " + reader["Family"] + "\n";
+            }
+
+            con.Close();
+            lblUsers.Text = output;
+            File.WriteAllText("output.txt", output);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowTBUsers();
+            ShowGV();
+        }
+
+        private void ShowGV()
+        {
+            string orderby;
+            if (rdbID.Checked)
+                orderby = "ID";
+            else if (rdbName.Checked)
+                orderby = "NAME";
+            else
+                orderby = "Family";
+
+            string conStr = @"Data Source=E440\SQLEXPRESS;Initial Catalog=DBUsers;Integrated Security=True";
+            SqlConnection con = new SqlConnection(conStr);
+            SqlCommand comm = new SqlCommand(
+                 " SELECT * " +
+                 " FROM TBUsers " +
+                $" Order By {orderby}", con);
+            con.Open();//opt1
+            SqlDataReader reader = comm.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+            //GVUsers.AutoGenerateColumns = True;
+            GVUsers.DataSource = dt;
+            //GVUsers.Refresh();
+
+
+            con.Close();
+        }
+    }
+}
