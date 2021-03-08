22c7c87039773848dd1e7d9c1000fe6885534ffc
nirc7 <i2cs2013@gmail.com>
2021-03-08 11:59:18 +0200

    DS

---
+++ 04 08-03-2021 SQL DataSet/DataSetDemo/DataSetDemo/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace DataSetDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFamily = new System.Windows.Forms.TextBox();
+            this.btnUpdateDB = new System.Windows.Forms.Button();
+            this.btnDeleteWParams = new System.Windows.Forms.Button();
+            this.btnSPrValue = new System.Windows.Forms.Button();
+            this.btnSPrTable = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(110, 92);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(104, 35);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "SELECT";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(298, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(317, 327);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(110, 150);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(104, 35);
+            this.btnInsert.TabIndex = 2;
+            this.btnInsert.Text = "INSERT";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(110, 204);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(104, 35);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(110, 262);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(104, 35);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(150, 26);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 26);
+            this.txtID.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(84, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(280, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "NAME:";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(346, 26);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 26);
+            this.txtName.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(473, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "FAMILY";
+            // 
+            // txtFamily
+            // 
+            this.txtFamily.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFamily.Location = new System.Drawing.Point(545, 29);
+            this.txtFamily.Name = "txtFamily";
+            this.txtFamily.Size = new System.Drawing.Size(100, 26);
+            this.txtFamily.TabIndex = 9;
+            // 
+            // btnUpdateDB
+            // 
+            this.btnUpdateDB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateDB.Location = new System.Drawing.Point(650, 92);
+            this.btnUpdateDB.Name = "btnUpdateDB";
+            this.btnUpdateDB.Size = new System.Drawing.Size(178, 35);
+            this.btnUpdateDB.TabIndex = 11;
+            this.btnUpdateDB.Text = "UPDATE DB";
+            this.btnUpdateDB.UseVisualStyleBackColor = true;
+            this.btnUpdateDB.Click += new System.EventHandler(this.btnUpdateDB_Click);
+            // 
+            // btnDeleteWParams
+            // 
+            this.btnDeleteWParams.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteWParams.Location = new System.Drawing.Point(650, 169);
+            this.btnDeleteWParams.Name = "btnDeleteWParams";
+            this.btnDeleteWParams.Size = new System.Drawing.Size(178, 35);
+            this.btnDeleteWParams.TabIndex = 12;
+            this.btnDeleteWParams.Text = "DELETE W Params";
+            this.btnDeleteWParams.UseVisualStyleBackColor = true;
+            this.btnDeleteWParams.Click += new System.EventHandler(this.btnDeleteWParams_Click);
+            // 
+            // btnSPrValue
+            // 
+            this.btnSPrValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSPrValue.Location = new System.Drawing.Point(650, 239);
+            this.btnSPrValue.Name = "btnSPrValue";
+            this.btnSPrValue.Size = new System.Drawing.Size(178, 35);
+            this.btnSPrValue.TabIndex = 13;
+            this.btnSPrValue.Text = "SP Return Value";
+            this.btnSPrValue.UseVisualStyleBackColor = true;
+            this.btnSPrValue.Click += new System.EventHandler(this.btnSPrValue_Click);
+            // 
+            // btnSPrTable
+            // 
+            this.btnSPrTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSPrTable.Location = new System.Drawing.Point(650, 303);
+            this.btnSPrTable.Name = "btnSPrTable";
+            this.btnSPrTable.Size = new System.Drawing.Size(178, 35);
+            this.btnSPrTable.TabIndex = 14;
+            this.btnSPrTable.Text = "SP Return Table";
+            this.btnSPrTable.UseVisualStyleBackColor = true;
+            this.btnSPrTable.Click += new System.EventHandler(this.btnSPrTable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 450);
+            this.Controls.Add(this.btnSPrTable);
+            this.Controls.Add(this.btnSPrValue);
+            this.Controls.Add(this.btnDeleteWParams);
+            this.Controls.Add(this.btnUpdateDB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFamily);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFamily;
+        private System.Windows.Forms.Button btnUpdateDB;
+        private System.Windows.Forms.Button btnDeleteWParams;
+        private System.Windows.Forms.Button btnSPrValue;
+        private System.Windows.Forms.Button btnSPrTable;
+    }
+}
+
---
+++ 04 08-03-2021 SQL DataSet/DataSetDemo/DataSetDemo/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSetDemo
+{
+    public partial class Form1 : Form
+    {
+        string strCon = @"Data Source=E440\SQLEXPRESS;Initial Catalog=DBUsers;Integrated Security=True";
+        SqlConnection con;
+        SqlDataAdapter adptr;
+        DataSet dsUsers;
+        DataTable dtUsers;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dsUsers = new DataSet();
+
+            con = new SqlConnection(strCon);
+            adptr = new SqlDataAdapter(
+                " SELECT * " +
+                " FROM TBUsers " +
+                " Order By Name ", con);
+
+            FillDSAndDT();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            FillDSAndDT();
+        }
+
+        private void FillDSAndDT()
+        {
+            dsUsers.Clear();
+            adptr.Fill(dsUsers, "Users1");
+            dtUsers = dsUsers.Tables["Users1"];
+
+            //MessageBox.Show(dtUsers.Rows[0][1].ToString());
+            dataGridView1.DataSource = dtUsers;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            DataRow dr = dtUsers.NewRow();
+            dr["Name"] = txtName.Text;
+            dr["Family"] = txtFamily.Text;
+            dr["ID"] = 7;
+
+            dtUsers.Rows.Add(dr);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            foreach (DataRow row in dtUsers.Rows)
+            {
+                if (row.RowState != DataRowState.Deleted && row["ID"].ToString() == txtID.Text)
+                {
+                    row["Name"] = txtName.Text;
+                    row["Family"] = txtFamily.Text;
+                    row["ID"] = 777;
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            foreach (DataRow row in dtUsers.Rows)
+            {
+                if (row.RowState != DataRowState.Deleted && row["ID"].ToString() == txtID.Text)
+                {
+                    row.Delete();
+                }
+            }
+        }
+
+        private void btnUpdateDB_Click(object sender, EventArgs e)
+        {
+            new SqlCommandBuilder(adptr);
+            adptr.Update(dtUsers);
+        }
+
+        private void btnDeleteWParams_Click(object sender, EventArgs e)
+        {
+            SqlCommand comm = new SqlCommand(
+                " DELETE From TBUsers " +
+                " WHERE ID = @ParID", con);
+            
+            //SqlParameter parI = new SqlParameter("ParID", txtID.Text);
+            //comm.Parameters.Add(parI);
+
+            //SqlParameter parI2 = new SqlParameter("ParID", SqlDbType.Int);
+            //parI2.Value = txtID.Text;
+
+            comm.Parameters.Add(new SqlParameter("ParID", txtID.Text));
+
+            comm.Connection.Open();
+            int res = comm.ExecuteNonQuery();
+            comm.Connection.Close();
+
+            MessageBox.Show(res.ToString());
+        }
+
+        private void btnSPrValue_Click(object sender, EventArgs e)
+        {
+            SqlCommand comm = new SqlCommand("SearchUser", con);
+            comm.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter parID = new SqlParameter("MyID", txtID.Text);
+            parID.Direction = ParameterDirection.Input;
+            comm.Parameters.Add(parID);
+
+            SqlParameter parFamilyName = new SqlParameter("FamilyName",SqlDbType.VarChar,20);
+            parFamilyName.Direction = ParameterDirection.Output;
+            comm.Parameters.Add(parFamilyName);
+
+            SqlParameter parErr = new SqlParameter();
+            parErr.Direction = ParameterDirection.ReturnValue;
+            comm.Parameters.Add(parErr);
+
+            comm.Connection.Open();
+            comm.ExecuteNonQuery();
+            comm.Connection.Close();
+
+            if ((int)parErr.Value == 0)
+                MessageBox.Show(parFamilyName.Value.ToString());
+            else
+                MessageBox.Show("ERROR OCCURDED");   
+        }
+
+        private void btnSPrTable_Click(object sender, EventArgs e)
+        {
+            SqlCommand comm = new SqlCommand("SearchUserTable", con);
+            comm.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter parID = new SqlParameter("MyID", txtID.Text);
+            parID.Direction = ParameterDirection.Input;
+            comm.Parameters.Add(parID);
+                       
+            SqlDataAdapter aptr2 = new SqlDataAdapter(comm);
+            DataSet ds2 = new DataSet();
+            aptr2.Fill(ds2, "T1");
+
+            dataGridView1.DataSource = ds2.Tables["T1"];
+        }
+    }
+}
